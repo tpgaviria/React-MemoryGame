b8864dfa73957263cb48e3e7f991f60f52f66164
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-07-15 12:40:48 -0400

    Updates

---
+++ precache-manifest.dd1cb407795624c5eb433635ce092cb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea77d1201099bf9388b99e678c672c64",
+    "url": "/react-memorygame/index.html"
+  },
+  {
+    "revision": "6ec295f3657b51483325",
+    "url": "/react-memorygame/static/css/main.3e0d0884.chunk.css"
+  },
+  {
+    "revision": "ed67e21e0fbb680b5f8f",
+    "url": "/react-memorygame/static/js/2.abafc83a.chunk.js"
+  },
+  {
+    "revision": "6ec295f3657b51483325",
+    "url": "/react-memorygame/static/js/main.d6f43624.chunk.js"
+  },
+  {
+    "revision": "6060fd71ca47dd8b25b5",
+    "url": "/react-memorygame/static/js/runtime~main.6c61113c.js"
+  }
+]);
